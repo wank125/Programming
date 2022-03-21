c147d689e063caed07072f5d1e2eed446ee79709
wank125 <30768017+wank125@users.noreply.github.com>
2022-03-21 18:04:51 +0800

    add

---
+++ programming/src/main/java/com/mike/base/SuperMath.java
@@ -0,0 +1,24 @@
+package com.mike.base;
+
+public class SuperMath {
+
+  public <T extends Comparable<T>> T getMaxMin(T[] num) {
+    T max = num[0];
+    T min = num[0];
+
+    for (int i = 0; i < num.length; i++) {
+      if (max.compareTo(num[i]) < 0) {
+        max = num[i];
+      }
+    }
+
+    for (int i = 0; i < num.length; i++) {
+      if (min.compareTo(num[i]) > 0) {
+        min = num[i];
+      }
+    }
+
+    return
+
+  }
+}
