1018ca08068c02920913f2c0115f5712d6cebca3
wank125 <30768017+wank125@users.noreply.github.com>
2022-04-02 10:43:54 +0800

    客户端互聊原理与实现

--- programming/src/main/java/com/mike/netty/attribute/Attributes.java
+++ programming/src/main/java/com/mike/netty/attribute/Attributes.java
@@ -4,6 +4,6 @@ import com.mike.netty.server.session.Session;
 import io.netty.util.AttributeKey;
 
 public interface Attributes {
-    //AttributeKey<Boolean> LOGIN = AttributeKey.newInstance("login");
+    AttributeKey<Boolean> LOGIN = AttributeKey.newInstance("login");
     AttributeKey<Session> SESSION = AttributeKey.newInstance("session");
 }
--- programming/src/main/java/com/mike/netty/client/handler/LoginResponseHandler.java
+++ programming/src/main/java/com/mike/netty/client/handler/LoginResponseHandler.java
@@ -1,10 +1,9 @@
 package com.mike.netty.client.handler;
 
-import com.mike.netty.protocol.PacketCodeC;
 import com.mike.netty.protocol.request.LoginRequestPacket;
 import com.mike.netty.protocol.response.LoginResponsePacket;
-import com.mike.netty.util.LoginUtil;
-import io.netty.buffer.ByteBuf;
+import com.mike.netty.server.session.Session;
+import com.mike.netty.server.session.SessionUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
@@ -14,21 +13,21 @@ import java.util.UUID;
 public class LoginResponseHandler extends SimpleChannelInboundHandler<LoginResponsePacket> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, LoginResponsePacket loginResponsePacket) throws Exception {
+        String userId = loginResponsePacket.getUserId();
+        String userName = loginResponsePacket.getUserName();
+
         if (loginResponsePacket.isSuccess()) {
             System.out.println(new Date() + ": 客户端登录成功");
-           // LoginUtil.markAsLogin(ctx.channel());
+            // LoginUtil.markAsLogin(ctx.channel());
+            SessionUtil.bindSession(new Session(userId, userName), ctx.channel());
         } else {
             System.out.println(new Date() + ": 客户端登录失败，原因：" + loginResponsePacket.getReason());
         }
     }
 
     @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-
-        LoginRequestPacket loginRequestPacket = new LoginRequestPacket();
-        loginRequestPacket.setUserId(UUID.randomUUID().toString());
-        loginRequestPacket.setUsername("flash");
-        loginRequestPacket.setPassword("pwd");
-        ctx.channel().writeAndFlush(loginRequestPacket);
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        //super.channelInactive(ctx);
+        System.out.println("客户端连接被关闭!");
     }
 }
